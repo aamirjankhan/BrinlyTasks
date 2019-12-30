70a34c5937d26b79250fbe03179c57ba968ee045
aamirjankhan <aamir.jankhan445@gmail.com>
2019-12-30 10:06:04 +0500

    Brainly Python tasks...

---
+++ brain10.py
@@ -0,0 +1,22 @@
+import sys
+'''
+A superb number is an integer which is less than the sum of
+its proper divisors, excluding itself. Thus, 12 is a superb number because
+1, 2, 3, and 6 are its proper positive divisors and 1 + 2 + 3 + 4 + 6 > 12.
+The next superb number is 18 < 1 + 2 + 3 + 6 + 9.
+The next two superb numbers are 20 and 24.
+Write a function called superb that returns true if
+the argument passed to it is a superb number and false otherwise. 
+
+def suberb(num):
+    i=1
+    for i in range()
+'''
+    
+dictionary = {"A":4,"C":5}
+try:
+    print (dictionary["5"])
+except ValueError:
+    print('Problem with the dictionary')
+    sys.exit()
+            
